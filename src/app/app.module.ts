4f7c523639b41073b0dc308050cfab83a10a9282
Sri Venkata Satya Sai Tokala <61743801+tsvss@users.noreply.github.com>
2021-03-17 18:26:46 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { DetailsPageComponent } from './details-page/details-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    DetailsPageComponent,
+    ContactPageComponent
   ],
   imports: [
     BrowserModule,
